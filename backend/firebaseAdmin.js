c8ec847c54afb715d912e6c3355dd3dfb4cf07d5
Falis Mohamed <falizmohamed85@gmail.com>
2026-05-27 16:08:47 +0300

    fix firebase uppercase env

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -2,16 +2,23 @@ const admin = require("firebase-admin");
 
 const serviceAccount = {
   type: "service_account",
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key?.replace(/\\n/g, "\n"),
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
-  universe_domain: process.env.universe_domain,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri:
+    process.env.FIREBASE_AUTH_URI ||
+    "https://accounts.google.com/o/oauth2/auth",
+  token_uri:
+    process.env.FIREBASE_TOKEN_URI ||
+    "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_AUTH_PROVIDER_CERT_URL ||
+    "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain:
+    process.env.FIREBASE_UNIVERSE_DOMAIN || "googleapis.com",
 };
 
 if (!admin.apps.length) {
