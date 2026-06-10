1c651c4d02880be32b8671c3d0e6593cdf41d429
Falis Mohamed <falizmohamed85@gmail.com>
2026-06-10 15:01:12 +0300

    fix gmail smtp connection

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,12 +3,14 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, payload) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     let text = "";
     let html = "";
